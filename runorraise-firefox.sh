a8ee7ec6765d6c4b6bfdf4120694ed073be8819e
Jan-Eric Schäfrich <164321999+janschafrich@users.noreply.github.com>
2025-01-15 21:41:22 +0100

    solved an issue where always a new instance would be launched

--- runorraise-firefox.sh
+++ runorraise-firefox.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# if calculator is not running, launch a new instance
-# if calculator is already running focus the existing instance
+# if program is not running, launch a new instance
+# if program is already running focus the existing instance
 
 pidof firefox
 
@@ -9,11 +9,12 @@ then
     firefox
 else
 # uses this: https://extensions.gnome.org/extension/5021/activate-window-by-title/
+# https://github.com/lucaswerkmeister/activate-window-by-title
     busctl --user call \
     org.gnome.Shell \
     /de/lucaswerkmeister/ActivateWindowByTitle \
     de.lucaswerkmeister.ActivateWindowByTitle \
-    activateBySubstring \
-    s 'Firefox'
+    activateByWmClass \
+    s 'firefox'
 
 fi
